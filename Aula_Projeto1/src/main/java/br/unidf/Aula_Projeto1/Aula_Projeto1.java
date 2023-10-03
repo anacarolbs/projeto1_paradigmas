e8a0d6aa3232f8ce441ec293c6558532289ee3eb
Ana Carolina Barbosa de Souza <souzaanacarolb@gmail.com>
2023-10-02 23:46:53 -0300

    Add files via upload

---
+++ Aula_Projeto1/src/main/java/br/unidf/Aula_Projeto1/Aula_Projeto1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package br.unidf.Aula_Projeto1;
+
+/**
+ *
+ * @author anacarol
+ */
+public class Aula_Projeto1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
